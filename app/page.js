3dab64729a6401690ececf19d2c52af39717ef72
Suraj Kumar <kumarsuraj841504@gmail.com>
2025-09-04 14:19:36 +0530

    folter structure

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
+import img from '@/assests/img/image1.jpg'
+import img2 from '@/assests/img/excited.gif'
+
 
 const page = () => {
   return (
-    <div>Lets's Begin !</div>
+    <div className='flex items-center mt-10 ml-5 gap-5'>
+    <div className='w-[50vw] h-[50vh]'><Image  src={img}  alt='img'/></div>
+    <div className='flex flex-col justify-center '><div className='text-6xl'>Lets's Begin !</div>
+    <div className='pt-10'><Image src={img2}/></div>
+    </div>
+    </div>
   )
 }
 

d8d4a925c9fd95c583228bfbc7954247ada640e9
Suraj Kumar <kumarsuraj841504@gmail.com>
2025-09-04 14:24:28 +0530

    Folder structure

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ const page = () => {
   return (
     <div className='flex items-center mt-10 ml-5 gap-5'>
     <div className='w-[50vw] h-[50vh]'><Image  src={img}  alt='img'/></div>
-    <div className='flex flex-col justify-center '><div className='text-6xl'>Lets's Begin !</div>
-    <div className='pt-10'><Image src={img2}/></div>
+    <div className='flex flex-col justify-center '><div className='text-8xl'>Lets's Begin !</div>
+    <div className='pt-10'><Image src={img2} alt='img2'/></div>
     </div>
     </div>
   )

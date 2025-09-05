6d0fce2cb15d4d9ddbac5335681fc8447bb764e7
twinkle <twinklekumari246@gmail.com>
2025-09-05 21:39:39 +0530

    Added statistics component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import SampleComponent from "@/app/_components/SampleComponent";
 import Hero from "@/app/_components/Hero";
+import { Stats } from "fs";
+import Statistics from "./_components/Statistics";
 export default function Home() 
 {
   return (
     <div>
       <Hero/>
       <SampleComponent/>
+      <Statistics/>
     </div>
   )
 }
